17de7e697bf9c5a83ccdc23af0295f086294baa3
tzyn <tzyn.wang@gmail.com>
2021-10-14 20:52:34 +0800

    chore: remove src/router testing content

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
-import test from '@/views/Test.vue'
 
 Vue.use(VueRouter)
 
@@ -10,11 +9,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/test',
-    name: 'test',
-    component: test
   }
 ]
 
